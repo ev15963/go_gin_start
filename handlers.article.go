fc0f0222b1086244f5d20ae1037008a026c07b81
ev15963 <ev15963@hs.ac.kr>
2023-02-21 07:44:49 +0900

    artticle test 생성

---
+++ handlers.article.go
@@ -0,0 +1,20 @@
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func showIndexPage(c *gin.Content) {
+// 	articles := getAllArticles()
+
+// 	c.HTML(
+// 		http.StatusOK,
+// 		"index.html",
+// 		gin.H{
+// 			"title":   "Home Page",
+// 			"payload": articles,
+// 		},
+// 	)
+// }
