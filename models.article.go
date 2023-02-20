0bbf144f1a4373547fa467dd64d5c9be98d6e3c9
ev15963 <ev15963@hs.ac.kr>
2023-02-21 06:51:25 +0900

    모델 생생

--- models.article.go
+++ models.article.go
@@ -2,12 +2,14 @@ package main
 
 import "log"
 
+// 게시글 타입
 type article struct {
 	ID      int    `json:"id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
+// 데이터 베이스 hard-coded
 var articleList = []article{
 	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
 	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
